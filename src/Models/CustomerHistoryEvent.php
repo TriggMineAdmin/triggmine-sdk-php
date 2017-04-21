7fe5c53bb32156fb703e743b53e2d1891c6ffd35
TriggMine Devs <admin@triggmine.com>
2017-04-21 12:57:10 +0000

    split HistoryEvents classes into separate files

---
+++ src/Models/CustomerHistoryEvent.php
@@ -0,0 +1,7 @@
+<?php
+namespace TriggMineSDK\Models;
+
+class CustomerHistoryEvent
+{
+    public $prospects;
+}
---
+++ src/Models/OrderHistoryEvent.php
@@ -0,0 +1,7 @@
+<?php
+namespace TriggMineSDK\Models;
+
+class OrderHistoryEvent
+{
+    public $orders;
+}
